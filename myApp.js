e4de630f916fdcc6e1cdc52a7d85a7acb28a9cb1
Elena Surilova <elen21surilova@gmail.com>
2022-09-19 13:59:15 +0300

    Static files, json

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 let express = require('express');
+const path = require('path');
 let app = express();
-console.log('Hello World');
 
+app.get('/', (req, res) => {
+  let absolutePath = path.join(__dirname, 'views/index.html');
+  res.sendFile(absolutePath);
+});
 
+let publicPath = path.join(__dirname, 'public');
+app.use('/public', express.static(publicPath));
+
+app.get('/json', (req, res) => {
+  res.json({ "message": "Hello json" });
+});
 
 
 
